42ffe3a3a80771b9a7ba6839c18931368abcb794
Jacques <jacques.verre@stakion.io>
2024-04-27 17:36:51 +0100

    Configure Github Actions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://deep-learning-glossary.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'jverre', // Usually your GitHub org/user name.
   projectName: 'deep-learning-glossary', // Usually your repo name.
   deploymentBranch: 'main',
-  trailingSlash: true,
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
